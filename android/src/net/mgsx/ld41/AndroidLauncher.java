a54c62c799777a4d9997e1c45c22c766e9f01437
Germain AUBERT <germain.aubert@gmail.com>
2018-04-21 10:16:47 +0200

    init from template

---
+++ android/src/net/mgsx/ld41/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package net.mgsx.ld41;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import net.mgsx.ld41.LD41;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LD41(), config);
+	}
+}
